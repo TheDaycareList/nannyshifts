56f44995f7437e08256818c33b66396238a3e179
Dave Coffin <dave@nimbit.com>
2017-09-14 16:31:07 -0400

    removing send grid key

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 export default class Config {
     static apiUrl = 'https://nanny-shifts.firebaseio.com';
-    static sendGridApiKey = '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';
+    static sendGridApiKey = '';
 }
